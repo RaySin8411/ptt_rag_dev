3ca11aebc13b10971e77beeeb2cc267801879613
raysin8411 <q8977452@gmail.com>
2026-02-26 16:56:12 +0800

    (folder) adjust items

--- app/settings.py
+++ app/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ptt_rag_dev.urls'
+ROOT_URLCONF = 'ptt_rag_dev.app.urls'
 
 TEMPLATES = [
     {
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ptt_rag_dev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_database',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
