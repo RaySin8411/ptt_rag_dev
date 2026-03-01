4a3e05e64b9d43d5d3d148fd285cf469cf3cd3d8
raysin8411 <q8977452@gmail.com>
2026-03-01 20:13:21 +0800

    (api-get) ListView, DetailView and StatisticsView

--- article/urls.py
+++ article/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.ArticleListView.as_view(), name='article-list'),
+    path('posts/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
+    path('statistics/', views.ArticleStatisticsView.as_view(), name='article-statistics'),
 ]
